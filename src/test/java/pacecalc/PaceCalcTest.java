8df10da9d6824e6cac991e5bc65c4e7077fc83c5
Oleksandr Molodetskyi <molodetskyidev@gmail.com>
2017-11-12 21:28:05 +0200

    added test for second constractor of PaceCalc

--- src/test/java/pacecalc/PaceCalcTest.java
+++ src/test/java/pacecalc/PaceCalcTest.java
@@ -7,37 +7,45 @@ public class PaceCalcTest {
 
 	PaceCalc mycalc = new PaceCalc(); // declare and initiate PaceCalc which
 										// will be tested
+	int distance, time, pace;
+	double speed;
 
 	@Test
+
+	// test for get and set methods
+
 	public void gettersSettersTest() {
 
 		// test set and get for distance
 
 		mycalc.setDistance(2000);
-		int distance = mycalc.getDistance();
+		distance = mycalc.getDistance();
 		Assert.assertEquals(distance, 2000, "Wrong distance!");
 
 		// test set and get for time
 
 		mycalc.setTime(3600);
-		int time = mycalc.getTime();
+		time = mycalc.getTime();
 		Assert.assertEquals(time, 3600, "Wrong time!");
 
 		// test set and get for pace
 
 		mycalc.setPace(360);
-		int pace = mycalc.getPace();
+		pace = mycalc.getPace();
 		Assert.assertEquals(pace, 360, "Wrong pace!");
 
 		// test set and get for speed
 
 		mycalc.setSpeed(5.34);
-		double speed = mycalc.getSpeed();
+		speed = mycalc.getSpeed();
 		Assert.assertEquals(speed, 5.34, "Wrong speed!");
 
 	}
 
 	@Test
+
+	// test for paceCalc method
+
 	public void paceCalcTest() {
 		int pace;
 
@@ -67,6 +75,37 @@ public class PaceCalcTest {
 		mycalc.setTime(1);
 		pace = mycalc.calcPace();
 		Assert.assertEquals(pace, 0, "Test halfmarathon by 1 hour Wrong pace! ");
+
+	}
+
+	@Test
+
+	// test for second constructor of PaceCalc = PaceCalc(int pace, int
+	// distance, int time, double speed)
+
+	public void secondConstructorTest() {
+		// check constructor PaceCalc(int pace, int distance, int time, double
+		// speed)
+		PaceCalc mycalcSecondConst = new PaceCalc(276, 30034, 42781, 12.7);
+
+		// check distance which is set by constructor
+		distance = mycalcSecondConst.getDistance();
+		Assert.assertEquals(distance, 30034, "Wrong distance! ");
+
+		// check time which is set by constructor
+
+		time = mycalcSecondConst.getTime();
+		Assert.assertEquals(time, 42781, "Wrong time! ");
+
+		// check time which is set by constructor
+
+		pace = mycalcSecondConst.getPace();
+		Assert.assertEquals(pace, 276, "Wrong pace! ");
+
+		// check speed which is set by constructor
+
+		speed = mycalcSecondConst.getSpeed();
+		Assert.assertEquals(speed, 12.7, "Wrong speed! ");
 
 	}
 }
