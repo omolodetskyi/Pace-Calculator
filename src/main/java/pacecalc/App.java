5346d2b851016d5149f1b972cdce05c0fb40a3f3
Oleksandr Molodetskyi <molodetskyidev@gmail.com>
2017-11-14 23:27:15 +0200

    modified App to run cli only in case of -cli parameter

--- src/main/java/pacecalc/App.java
+++ src/main/java/pacecalc/App.java
@@ -2,9 +2,21 @@ package pacecalc;
 
 public class App {
 	public static void main(String[] args) {
-		Controller controller = new Controller();
-		CLUI cli = new CLUI();
-		PaceCalc pacecalc = new PaceCalc();
-		controller.CalcFlow(pacecalc, cli);
+		try {
+			if (args[0].equals("-cli")) {
+				Controller controller = new Controller();
+				CLUI cli = new CLUI();
+				PaceCalc pacecalc = new PaceCalc();
+				controller.CalcFlow(pacecalc, cli);
+			}
+
+			else {
+				// not implemented yet
+			}
+
+		} catch (ArrayIndexOutOfBoundsException e) {
+			// not implemented yet
+		}
+
 	}
 }
