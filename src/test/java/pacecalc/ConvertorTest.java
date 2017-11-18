5cd453b12c882d04e3f5b42aaaad72718f435f09
Oleksandr Molodetskyi <molodetskyidev@gmail.com>
2017-11-18 23:22:18 +0200

    moved parameters for ConvertorTest to dataProvider

--- src/test/java/pacecalc/ConvertorTest.java
+++ src/test/java/pacecalc/ConvertorTest.java
@@ -1,6 +1,7 @@
 package pacecalc;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import util.Convertor;
@@ -13,83 +14,112 @@ public class ConvertorTest {
 	double speedKmH;
 	String speedMpH;
 
-	@Test
+	@Test(dataProvider = "setPrametersForConvertKmtoMileTest")
 
 	// test method which convert km to mile
 
-	public void convertKMtoMileTest() {
+	public void convertKMtoMileTest(int distanceKm, String distanceMileExpected, String message) {
 
-		// usual distance
-
-		distanceKm = 10000;
 		distanceMile = convertor.convertKmToMile(distanceKm);
-		Assert.assertEquals(distanceMile, "6.214", "Test converting usual distance km to mile");
-
-		// small distance
-		distanceKm = 10;
-		distanceMile = convertor.convertKmToMile(distanceKm);
-		Assert.assertEquals(distanceMile, "0.006", "Test converting usual distance km to mile");
-
-		// big distance
-		distanceKm = 10000000;
-		distanceMile = convertor.convertKmToMile(distanceKm);
-		Assert.assertEquals(distanceMile, "6214.0", "Test converting usual distance km to mile");
+		Assert.assertEquals(distanceMile, distanceMileExpected, message);
 
 	}
 
-	@Test
+	@DataProvider(name = "setPrametersForConvertKmtoMileTest")
+	private Object[][] setPrametersForConvertKmtoMileTest() {
+		Object[][] params = new Object[3][3];
+
+		// usual distance
+
+		params[0][0] = 10000;
+		params[0][1] = "6.214";
+		params[0][2] = "Test converting usual distance km to mile";
+
+		// small distance
+
+		params[1][0] = 10;
+		params[1][1] = "0.006";
+		params[1][2] = "Test converting small distance km to mile";
+
+		// big distance
+
+		params[2][0] = 10000000;
+		params[2][1] = "6214.0";
+		params[2][2] = "Test converting big distance km to mile";
+
+		return params;
+	}
+
+	@Test(dataProvider = "setPrametersForConvertMileToKmTest")
 
 	// test method which convert mile to km
 
-	public void convertMileToKmTest() {
+	public void convertMileToKmTest(int distanceKmExpected, String distanceMile, String message) {
 
-		// usual distance
-
-		distanceMile = "6.214";
 		distanceKm = convertor.convertMileToKm(distanceMile);
-		Assert.assertEquals(distanceKm, 9999, "Test converting usual distance mile to km");
-
-		// small distance
-
-		distanceMile = "0.006";
-		distanceKm = convertor.convertMileToKm(distanceMile);
-		Assert.assertEquals(distanceKm, 10, "Test converting usual distance mile to km");
-
-		// big distance
-		// TODO should think about precise, in this particular case I've lost
-		// 1674 m - almost 1 mile
-
-		distanceMile = "6214.0";
-		distanceKm = convertor.convertMileToKm(distanceMile);
-		Assert.assertEquals(distanceKm, 9998326, "Test converting usual distance mile to km");
+		Assert.assertEquals(distanceKm, distanceKmExpected, message);
 
 	}
 
-	@Test
+	@DataProvider(name = "setPrametersForConvertMileToKmTest")
+	private Object[][] setPrametersForConvertMileToKmTest() {
+		Object[][] params = new Object[3][3];
+
+		// usual distance
+
+		params[0][0] = 9999;
+		params[0][1] = "6.214";
+		params[0][2] = "Test converting usual distance mile to km";
+
+		// small distance
+
+		params[1][0] = 10;
+		params[1][1] = "0.006";
+		params[1][2] = "Test converting usual distance mile to km";
+
+		// big distance
+
+		params[2][0] = 9998326;
+		params[2][1] = "6214.0";
+		params[2][2] = "Test converting usual distance mile to km";
+
+		return params;
+	}
+
+	@Test(dataProvider = "setPrametersForConvertSpeedKmToMileTest")
 
 	// test method which convert km/h to mile/h
 
-	public void convertSpeedKmToMileTest() {
+	public void convertSpeedKmToMileTest(double speedKmH, String speedMpHExpected, String message) {
+		speedMpH = convertor.convertSpeedKmhToMpH(speedKmH);
+		Assert.assertEquals(speedMpH, speedMpHExpected, message);
+	}
+
+	@DataProvider(name = "setPrametersForConvertSpeedKmToMileTest")
+	private Object[][] setPrametersForConvertSpeedKmToMileTest() {
+		Object[][] params = new Object[3][3];
 
 		// usual speed
 
-		speedKmH = 10.0;
-		speedMpH = convertor.convertSpeedKmhToMpH(speedKmH);
-		Assert.assertEquals(speedMpH, "6.214", "Test converting usual speed kmh to mph");
+		params[0][0] = 10.0;
+		params[0][1] = "6.214";
+		params[0][2] = "Test converting usual speed kmh to mph";
 
 		// small speed
 
-		speedKmH = 0.01;
-		speedMpH = convertor.convertSpeedKmhToMpH(speedKmH);
-		Assert.assertEquals(speedMpH, "0.007", "Test converting small speed kmh to mph");
+		params[1][0] = 0.01;
+		params[1][1] = "0.007";
+		params[1][2] = "Test converting small speed kmh to mph";
 
 		// big speed
 		// TODO should think about precise, in this particular case I've lost
 		// 1674 m - almost 1 mile
 
-		speedKmH = 10000.0;
-		speedMpH = convertor.convertSpeedKmhToMpH(speedKmH);
-		Assert.assertEquals(speedMpH, "6214.0", "Test converting big speed mile to km");
+		params[2][0] = 10000.0;
+		params[2][1] = "6214.0";
+		params[2][2] = "Test converting big speed mile to km";
+
+		return params;
 	}
 
 	@Test
