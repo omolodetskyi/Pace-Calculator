84ce3f29d274d7fe970ca23a174665537b8580d5
Oleksandr Molodetskyi <molodetskyidev@gmail.com>
2018-01-13 20:10:31 +0200

    refactored main method to simplify it by extracting runGUI and runCLI methods

--- src/main/java/pacecalc/App.java
+++ src/main/java/pacecalc/App.java
@@ -4,25 +4,32 @@ public class App {
 	public static void main(String[] args) {
 		try {
 			if (args[0].equals("-cli")) {
-				Controller controller = new Controller();
-				CLUI cli = new CLUI();
-				PaceCalc pacecalc = new PaceCalc();
-				controller.CalcFlow(pacecalc, cli);
+				runCLI();
 			}
 
 			else {
-				Controller controller = new Controller();
-				GUI gui = new GUI();
-				PaceCalc pacecalc = new PaceCalc();
-				controller.CalcFlow(pacecalc, gui);
+				runGUI();
 			}
 
 		} catch (ArrayIndexOutOfBoundsException e) {
-			Controller controller = new Controller();
-			GUI gui = new GUI();
-			PaceCalc pacecalc = new PaceCalc();
-			controller.CalcFlow(pacecalc, gui);
+			runGUI();
 		}
 
+	}
+
+	// method to run CLI interface
+	private static void runCLI() {
+		Controller controller = new Controller();
+		CLUI cli = new CLUI();
+		PaceCalc pacecalc = new PaceCalc();
+		controller.CalcFlow(pacecalc, cli);
+	}
+
+	// method to run GUI interface
+	private static void runGUI() {
+		Controller controller = new Controller();
+		GUI gui = new GUI();
+		PaceCalc pacecalc = new PaceCalc();
+		controller.CalcFlow(pacecalc, gui);
 	}
 }
